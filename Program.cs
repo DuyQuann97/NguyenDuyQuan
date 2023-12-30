970ec394da26580deee7d117e04360abc53d4487
DQuan97 <ngduyquan97@gmail.com>
2023-12-30 18:29:54 +0700

    Update Code

--- Program.cs
+++ Program.cs
@@ -32,6 +32,9 @@ namespace ToDoList
                     case 4:
                         TimKiemViecLam();
                         break;
+                    case 5:
+                        HienThiTheoDoUuTien();
+                        break;
                     case 6:
                         HienThiToanBoViecLam();
                         break;
@@ -69,10 +72,18 @@ namespace ToDoList
             Console.Write("Ten viec can lam: ");
             string tenViecLam = Console.ReadLine();
             int doUuTien = 5;
-            Console.Write("Do uu tien: ");
             try 
             {
-                doUuTien = int.Parse(Console.ReadLine());
+                while (true)
+                {
+                    Console.Write("Do uu tien: ");
+                    doUuTien = int.Parse(Console.ReadLine());
+                    if (doUuTien > 0 && doUuTien <= 5)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Do Uu Tien tu 1 - 5 voi 1 la Uu Tien . Moi nhap lai !!") ;
+                }
             }
             catch (Exception ex)
             {
@@ -88,7 +99,6 @@ namespace ToDoList
             var viecCanlam = new ViecCanLam(tenViecLam, doUuTien, thongTinViecLam, trangThaiViecLam);
             listViecCanLam.Add(viecCanlam);
         }
-
         public static void XoaViecLam() 
         {
             string message = "Khong co viec lam de xoa";
@@ -96,13 +106,11 @@ namespace ToDoList
             {
                 var lastItem = listViecCanLam.Count();
                 listViecCanLam.RemoveAt(lastItem - 1);
-                message = " => Da duoc xoa thanh cong";
-                Console.WriteLine("Viec lam: {0}",listViecCanLam.LastOrDefault().tenViecCanLam) ;
+                message = " => Da duoc xoa thanh cong viec lam truoc do";
             }
             Console.WriteLine("{0}", message);
             Console.WriteLine();
         }
-
         public static void CapNhatTrangThai() 
         {
             string message = "Khong co viec lam de cap nhat";
@@ -141,10 +149,10 @@ namespace ToDoList
             Console.WriteLine("{0}", message);
             Console.WriteLine();
         }
-
-        public static void HienThiToanBoViecLam() 
+        public static void HienThiTheoDoUuTien() 
         {
-            foreach (var viecLam in listViecCanLam)
+            var listDoUuTien = listViecCanLam.OrderBy(c => c.doUuTien);
+            foreach (var viecLam in listDoUuTien)
             {
                 Console.WriteLine();
                 Console.WriteLine("Ten viec can lam: {0}", viecLam.tenViecCanLam);
@@ -153,6 +161,25 @@ namespace ToDoList
                 Console.WriteLine("Trang thai: {0}", viecLam.trangThaiViecCanLam);
                 Console.WriteLine();
             }
+        }
+        public static void HienThiToanBoViecLam() 
+        {
+            string message = "khong viec lam de hien thi";
+            if (listViecCanLam.Count > 0)
+            {
+                foreach (var viecLam in listViecCanLam)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Ten viec can lam: {0}", viecLam.tenViecCanLam);
+                    Console.WriteLine("Do uu tien: {0}", viecLam.doUuTien);
+                    Console.WriteLine("Thong tin chi tiet: {0}", viecLam.thongTinViecCanLam);
+                    Console.WriteLine("Trang thai: {0}", viecLam.trangThaiViecCanLam);
+                    Console.WriteLine();
+                }
+                message = null;
+            }
+            Console.WriteLine("{0}", message) ;
+            
         }
 
     }
